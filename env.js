c8b1ad8f787781e737f04f2e87a75f03d0fd9c35
Electricdogex <julianjhooper@gmail.com>
2025-09-21 10:23:07 -0700

    Create env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+// replace with your values
+window.ENV = {
+  SUPABASE_URL: "https://vgdskvrovkekoxkixzsh.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnZHNrdnJvdmtla294a2l4enNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NzIwOTYsImV4cCI6MjA3NDA0ODA5Nn0.e_wcEufQiUQpVwyAcshfOOdwf48gGzP8RiFOu5GYYmo"
+};
